0c884434faf821ae933b49d26d42b350209b1d21
LoCHao <ch389212@gmail.com>
2019-04-17 23:56:34 +0800

    Update 104_maximumDepthBT.cpp

--- easy/104_maximumDepthBT.cpp
+++ easy/104_maximumDepthBT.cpp
@@ -37,5 +37,16 @@ public:
           return max(maxDepth(root->left),maxDepth(root->right))+1;
       
     }
-      
+     
+    //c++ cin 與 stdin 會同步讀取導致 cin 速度慢
+    ios::sync_with_stdio(false); //取消同步
+    std::cin.tie(0);//解除 cin cout 的綁定
+    //參考寫法
+    static auto speedup = [](){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        return nullptr;
+    }();
+    
+    
 };
